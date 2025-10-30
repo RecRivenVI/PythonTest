58adf9b9cd812a57734d0e844ac9ada829d6a156
Raven Yin <178878631@qq.com>
2025-10-30 18:19:56 +0800

    Update pack_sundial_lite.ps1

--- pack_sundial_lite.ps1
+++ pack_sundial_lite.ps1
@@ -1,11 +1,14 @@
-$RepoPath = "D:\GitHub\Sundial-Lite"
-Set-Location $RepoPath
+Set-Location "D:\GitHub\Sundial-Lite"
+
 git checkout main
 git pull origin main
-$DevName = "GeForceLegend"
-$CommitDate = git log -1 --author=$DevName --pretty=format:"%ad" --date=format:'%Y-%m-%d'
-Write-Host "Last commit date of ${DevName}: $CommitDate"
-$ZipPath = "D:\Downloads\Sundial Lite Dev Packup $CommitDate.zip"
-if (Test-Path $ZipPath) { Remove-Item $ZipPath }
-Compress-Archive -Path ".\shaders" -DestinationPath $ZipPath
-Write-Host "ZIP file created at $ZipPath"
+
+$CommitDate = git log -1 --author="GeForceLegend" --pretty=format:"%ad" --date=format:'%Y-%m-%d'
+Write-Host "Last commit date of GeForceLegend: $CommitDate"
+
+if (Test-Path "D:\Downloads\Sundial Lite Dev Packup $CommitDate.zip") {
+    Remove-Item "D:\Downloads\Sundial Lite Dev Packup $CommitDate.zip"
+}
+
+Compress-Archive -Path ".\shaders" -DestinationPath "D:\Downloads\Sundial Lite Dev Packup $CommitDate.zip"
+Write-Host "ZIP file created at D:\Downloads\Sundial Lite Dev Packup $CommitDate.zip"
